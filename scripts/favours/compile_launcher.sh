4658e86ea7daefcdee1ffe74460d2d06d27afa2c
ChengHao <chengnangong@163.com>
2025-05-12 02:16:37 +0800

    feat: 更新编译脚本和参数网关以增强蜂鸣器功能

--- scripts/favours/compile_launcher.sh
+++ scripts/favours/compile_launcher.sh
@@ -2,6 +2,12 @@
 
 export DART_DEVICE_TYPE="dart_launcher"
 
+echo -e "\033[1;32mBuilding ROS2 packages for $DART_DEVICE_TYPE\033[0m"
+
+echo -e "\033[1;33mStopping dart_ros2_run.service\033[0m"
+sudo systemctl stop dart_ros2_run.service
+echo -e "\033[1;33mCompiling dart_ros2 packages\033[0m"
+
 colcon build --merge-install \
     --cmake-args \
     -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
@@ -10,3 +16,10 @@ colcon build --merge-install \
     --packages-select \
     dart_msgs dart_launcher \
     cv_bridge dart_detector
+
+if [ $? -ne 0 ]; then
+    echo -e "\033[1;31mFailed to build ROS2 packages\033[0m"
+    exit 1
+fi
+
+echo -e "\033[1;32mSuccessfully built ROS2 packages\033[0m"
--- src/dart_launcher/src/node_dart_param_gateway.cpp
+++ src/dart_launcher/src/node_dart_param_gateway.cpp
@@ -450,6 +450,8 @@ void NodeDartParamGateway::process_qr_code(const std_msgs::msg::String::SharedPt
 
             // 发布扬声器信息
             auto buzzer_msg = std_msgs::msg::Int32();
+            buzzer_msg.data = BuzzerSoundMax;
+            dart_buzzer_cmd_pub_->publish(buzzer_msg);
             buzzer_msg.data = BuzzerHaru;
             dart_buzzer_cmd_pub_->publish(buzzer_msg);
         }
